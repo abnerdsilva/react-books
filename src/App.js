f64a024b43a29a803415152220ecbaf255986fa9
abnerdsilva <abner.computadores@outlook.com>
2019-04-18 21:47:31 -0300

    add função de consulta API e add componente BooksAPI * ajustado tela com categorias usando maps

--- src/App.js
+++ src/App.js
@@ -2,15 +2,28 @@ import React from 'react'
 import { Route } from 'react-router-dom'
 import BookShelf from './BookShelf'
 import SearchBooks from './SearchBooks'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 class BooksApp extends React.Component {
   state = {
+    books: []
+  }
 
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.setState({ books })
+    })
   }
 
   render() {
+    const { books } = this.state // simplifica variavel para não precisar usar sempre o this.state
+
+    // CRIA VARIAVEL DE ACORDO COM O FILTRO
+    let currentlyReading = books.filter((book) => book.shelf === 'currentlyReading')
+    let wantToRead = books.filter((book) => book.shelf === 'wantToRead')
+    let read = books.filter((book) => book.shelf === 'read')
+
     return (
       <div className="app">
         <Route exact path="/search" render={() => (
@@ -23,8 +36,9 @@ class BooksApp extends React.Component {
               <h1>MyReads</h1>
             </div>
             <div className="list-books-content">
-              <BookShelf />
-            </div>
+              <BookShelf category={ 'Currently Reading' } books={ currentlyReading }/>
+              <BookShelf category={ 'Wait to Reading' } books={ wantToRead }/>
+              <BookShelf category={ 'Read' } books={ read }/>            </div>
           </div>
         )} />
 
