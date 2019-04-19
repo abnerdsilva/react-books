a4d23972703355b44bb65eeadb5b8493cf477fbb
abnerdsilva <abner.computadores@outlook.com>
2019-04-19 16:52:26 -0300

    correção de key em lista

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -8,17 +8,17 @@ class ListBooks extends Component {
             <div className="bookshelf-books">
                 <ol className="books-grid">
                 {books.map((book) => (
-                    <li key='book.title'>
+                    <li key={ book.id }>
                     <div className="book">
                         <div className="book-top">
                         <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
                         <div className="book-shelf-changer">
                             <select value={book.shelf ? book.shelf : 'none'} onChange={(event) => changeShelf(book, event.target.value)}>
-                            <option value="move" disabled>Move to...</option>
-                            <option value="currentlyReading">Currently Reading</option>
-                            <option value="wantToRead">Want to Read</option>
-                            <option value="read">Read</option>
-                            <option value="none">None</option>
+                                <option value="move" disabled>Move to...</option>
+                                <option value="currentlyReading">Currently Reading</option>
+                                <option value="wantToRead">Want to Read</option>
+                                <option value="read">Read</option>
+                                <option value="none">None</option>
                             </select>
                         </div>
                         </div>
