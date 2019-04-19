1ccb5094790a3fda4ab2d13111bed5b661ea555b
abnerdsilva <abner.computadores@outlook.com>
2019-04-18 21:30:02 -0300

    add função de route e configurado links

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Route } from 'react-router-dom'
 import BookShelf from './BookShelf'
 import SearchBooks from './SearchBooks'
 // import * as BooksAPI from './BooksAPI'
@@ -6,21 +7,17 @@ import './App.css'
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
+
   }
 
   render() {
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
+        <Route exact path="/search" render={() => (
           <SearchBooks />
-        ) : (
+        )} />
+
+        <Route exact path="/" render={() => (
           <div className="list-books">
             <div className="list-books-title">
               <h1>MyReads</h1>
@@ -28,9 +25,9 @@ class BooksApp extends React.Component {
             <div className="list-books-content">
               <BookShelf />
             </div>
-
           </div>
-        )}
+        )} />
+
       </div>
     )
   }
